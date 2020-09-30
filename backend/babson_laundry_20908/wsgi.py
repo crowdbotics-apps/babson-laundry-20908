11cb003de4a95d46d6b59f037c97f945cea39ec3
Crowdbotics <team@crowdbotics.com>
2020-09-30 23:32:50 +0000

    Update models

--- backend/babson_laundry_20908/wsgi.py
+++ backend/babson_laundry_20908/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'babson_laundry_20908.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "babson_laundry_20908.settings")
 
 application = get_wsgi_application()
